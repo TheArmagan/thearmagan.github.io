c97f1bafed4226d123a6fc5b10c8ab433a5e7d31
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2020-10-21 18:22:46 +0300

    change project loading system
    and add EasyImage project

---
+++ __MY_PROJECTS__.js
@@ -0,0 +1,85 @@
+const __MY_PROJECTS__ = [{
+        name: "JPATH",
+        description: "Kolay bir şekilde json objesinin içerisinde gezinmenizi sağlar!",
+        previewImage: "./images/previews/jpath.png",
+        buttons: [{
+                name: "Siteye Git",
+                url: "https://thearmagan.github.io/JPATH",
+            },
+            {
+                name: "Github",
+                url: "https://github.com/TheArmagan/JPATH",
+            }
+        ]
+    },
+    {
+        name: "DABOX",
+        description: "Sınırsız miktarda dosya yükleyebildiğiniz bir site!",
+        previewImage: "./images/previews/dabox.png",
+        buttons: [{
+                name: "Siteye Git",
+                url: "https://da-box.herokuapp.com/",
+            },
+            {
+                name: "Github",
+                url: "",
+            }
+        ]
+    },
+    {
+        name: "EasyImage",
+        description: "Kolay ve hızlı bir şekilde fotaraf görüntileyebilmenizi sağlayan bir uygulama!",
+        previewImage: "./images/previews/easyimage.png",
+        buttons: [{
+                name: "İNDİR",
+                url: "https://github.com/TheArmagan/EasyImage/releases/latest",
+            },
+            {
+                name: "Github",
+                url: "https://github.com/TheArmagan/EasyImage",
+            }
+        ]
+    },
+    {
+        name: "CLICKCOLOR",
+        description: "Gerçek zamanlı olarak renk değiştirebildiğiniz ve renk değiştiğinde POST requesti attırabildiğiniz bir site.",
+        previewImage: "./images/previews/clickcolor.png",
+        buttons: [{
+                name: "Siteye Git",
+                url: "https://clickcolor.herokuapp.com/",
+            },
+            {
+                name: "Github",
+                url: "https://github.com/TheArmagan/clickcolor",
+            }
+        ]
+    },
+    {
+        name: "GeForce Now - Supported Games",
+        description: "Nvidia GeForce NOW tarafından desteklenen oyunları listelemenizi ve filtrelemenizi sağlar.",
+        previewImage: "./images/previews/ngfsg.png",
+        buttons: [{
+                name: "Siteye Git",
+                url: "https://thearmagan.github.io/nvidia-geforce-now-supported-games",
+            },
+            {
+                name: "Github",
+                url: "https://github.com/TheArmagan/nvidia-geforce-now-supported-games",
+            }
+        ]
+    },
+    {
+        name: "DATAPIPE",
+        description: "HTTP/HTTPS Proxy with custom request headers.",
+        previewImage: "./images/previews/datapipe.png",
+        buttons: [{
+                name: "Siteye Git",
+                url: "https://kao-datapipe-1.herokuapp.com/",
+            },
+            {
+                name: "Github",
+                url: "https://github.com/TheArmagan/data-pipe",
+            }
+        ]
+    },
+]
--- script.js
+++ script.js
@@ -2,49 +2,13 @@ const app = new Vue({
     el: "#vue-app",
     data: {
         isAboutPage: false,
-        myProjects: [
-            {
-                name: "JPATH",
-                description: "Kolay bir şekilde json objesinin içerisinde gezinmenizi sağlar!",
-                previewURL: "./images/previews/jpath.png", 
-                siteURL: "https://thearmagan.github.io/JPATH",
-                githubURL: "https://github.com/TheArmagan/JPATH"
-            },
-            {
-                name: "DABOX",
-                description: "Sınırsız miktarda dosya yükleyebildiğiniz bir site!",
-                previewURL: "./images/previews/dabox.png", 
-                siteURL: "https://da-box.herokuapp.com/",
-                githubURL: ""
-            },
-            {
-                name: "CLICKCOLOR",
-                description: "Gerçek zamanlı olarak renk değiştirebildiğiniz ve renk değiştiğinde POST requesti attırabildiğiniz bir site.",
-                previewURL: "./images/previews/clickcolor.png", 
-                siteURL: "https://clickcolor.herokuapp.com/",
-                githubURL: "https://github.com/TheArmagan/clickcolor"
-            },
-            {
-                name: "GeForce Now - Supported Games",
-                description: "Nvidia GeForce NOW tarafından desteklenen oyunları listelemenizi ve filtrelemenizi sağlar.",
-                previewURL: "./images/previews/ngfsg.png", 
-                siteURL: "https://thearmagan.github.io/nvidia-geforce-now-supported-games",
-                githubURL: "https://github.com/TheArmagan/nvidia-geforce-now-supported-games"
-            },
-            {
-                name: "DATAPIPE",
-                description: "HTTP/HTTPS Proxy with custom request headers.",
-                previewURL: "./images/previews/datapipe.png", 
-                siteURL: "https://kao-datapipe-1.herokuapp.com/",
-                githubURL: "https://github.com/TheArmagan/data-pipe"
-            }
-        ]
+        myProjects: __MY_PROJECTS__
     },
     mounted: function () {
-        setTimeout(()=>{
-            requestAnimationFrame(()=>{
+        setTimeout(() => {
+            requestAnimationFrame(() => {
                 document.body.classList.remove("hidden");
-            })
-        },10);
-    }
-})
+            });
+        }, 10);
+    },
+});
